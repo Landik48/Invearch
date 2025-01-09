8d787879e13aba1a2145fce725633b5d8d7e2e10
Landik <landik48@list.ru>
2025-01-09 23:05:40 +0300

    Update app.vue: add header, animations

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,50 @@
 <script setup>
+import { onMounted, ref, useTemplateRef } from 'vue';
 
+const user = ref(null);
+const loading = ref(false)
+const load_el = useTemplateRef('loading')
+
+const getUser = async () => {
+  loading.value = true
+  try {
+    const response = await fetch('http://127.0.0.1:8000/api/users/user');
+    // неисправность с получением запрета на доступ к пользователю(не авторизован)
+    if (response.ok) {
+      const fetchedUser = await response.json();
+      user.value = fetchedUser;
+      console.log(user.value); 
+    } else if (response.status == 301) {
+
+    }
+  } catch {
+    alert("Ошибка на стороне сервера, попробуйте позже")
+  } 
+  let opacity = 1; 
+  const interval = setInterval(() => {
+    opacity -= 0.1;
+    if (opacity <= 0) {
+        clearInterval(interval);
+        loading.value = false
+    }
+    load_el.value.style.opacity = opacity;
+  }, 50);
+}
+
+onMounted(() => {
+  document.title = 'Главная';
+  getUser(); 
+});
 </script>
+
 
 <template>
   <nav>
     <RouterLink to="/" class="router-text">Главная</RouterLink>
     <RouterLink to="/startups" class="router-text">Стартапы</RouterLink>
-    <RouterLink to="/profile" class="router-text">Профиль</RouterLink>
-    <RouterLink to="/auth" class="router-text">Авторизация</RouterLink>
+    <RouterLink to="/profile" class="router-text smooth-appearance" v-if="user != null && !loading">Профиль</RouterLink>
+    <RouterLink to="/auth" class="router-text smooth-appearance" v-if="user == null && !loading">Авторизация</RouterLink>
+    <div class="loading router-text" v-if="loading" ref="loading">Проверка профиля...</div>
   </nav>
   <main>
     <RouterView />
